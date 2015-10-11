f610976dfd4f25ba769d50bd4b3646ac34e07b0a
Andrew Booth <adbooth8@gmail.com>
2015-10-10 20:24:10 -0400

    The deletes from the restructure

--- game/static/js/game.js
+++
@@ -1,6 +0,0 @@
-Crafty.init(window.innerWidth, window.innerHeight, document.getElementById('game'));
-
-Crafty.e('2D, DOM, Color, Fourway')
-    .attr({x: 0, y: 0, w: 100, h: 100})
-    .color('#F00')
-    .fourway(5);
