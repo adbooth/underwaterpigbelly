399d61687b2cbbbadfe6f655139949fa3b7cb5e9
Andrew Booth <adbooth8@gmail.com>
2015-10-12 21:10:58 -0400

    Client side game updates corresponding with class changes

--- application/static/js/game.js
+++ application/static/js/game.js
@@ -1,6 +1,6 @@
 Crafty.init(window.innerWidth, window.innerHeight, document.getElementById('game'));
 
-Crafty.e('2D, DOM, Color, Fourway')
+var player = Crafty.e('2D, DOM, Color, Fourway')
     .attr({x: 0, y: 0, w: 100, h: 100})
     .color('#F00')
     .fourway(5)
@@ -8,11 +8,11 @@ Crafty.e('2D, DOM, Color, Fourway')
         socket.emit('player_location', {'x': this.x, 'y': this.y});
     });
 
-socket.on('location_info', function(info){
-    // $('#x').text(info.x);
-    // $('#y').text(info.y);
-    var xtag = document.getElementById('x');
-    var ytag = document.getElementById('y');
-    xtag.innerHTML = 'x: ' + info.x;
-    ytag.innerHTML = 'y: ' + info.y;
-});
+function addPlayer(){
+    
+}
+
+// socket.on('location_info', function(info){
+//     var xtag = document.getElementById('x').innerHTML = 'x: ' + info.x;
+//     var ytag = document.getElementById('y').innerHTML = 'y: ' + info.y;
+// });
--- application/static/js/socket.js
+++ application/static/js/socket.js
@@ -3,13 +3,13 @@ var thisURL = location.protocol + '//' + document.domain + (location.port ? ':' 
 var socket = io.connect(thisURL);
 
 // When connected, start the handshake with an 'offer'
-socket.on('connect', function() {
+socket.on('connect', function(){
     socket.emit('handshake_from_client', {'key': 'hand'});
 });
 
 // Connection is confirmed
 socket.on('handshake_to_client', function(response) {
-    if (response.key == 'shake') {
+    if(response.key == 'shake'){
         alert('connected!');
     }
 });
